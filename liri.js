cd06ee48a4ffe4ab22a7f91df4eb6805ddca3536
Ben deLong <bendelong45@gmail.com>
2018-11-08 22:45:40 -0600

    updated comments on liri.js

--- liri.js
+++ liri.js
@@ -1,3 +1,4 @@
+// initializes variables that require dependencies
 require("dotenv").config();
 const Spotify = require("node-spotify-api");
 const fs = require("fs");
@@ -9,9 +10,9 @@ const ombdKey = keys.omdb;
 const bandsKey = keys.bands;
 let liriCommand = process.argv[2];
 let userInput = process.argv.slice(3).join(" ");
-
+// calls the function that kicks Liri off
 kickOff(liriCommand, userInput);
-
+// liri kick off function.  Has a switch case that calls functions based on what the user indicates.  Passes the user input into those functions.
 function kickOff(liriCommand, userInput) {
   switch (liriCommand) {
     case "spotify-this-song":
@@ -31,13 +32,16 @@ function kickOff(liriCommand, userInput) {
       break;
   }
 };
-
+// spotify function.
 function spotifyThisSong(songName) {
+  // if user doesn't specify a song, The Sign by Ace of Base is assigned to user's input.
   if (!songName) {
     songName = "The Sign, Ace of Base"
   };
+  // makes request to spotify
   spotify.search({ type: 'track', query: songName, limit: 10 }, function (err, data) {
     if (!err) {
+      // logs response
       let songInfo = data.tracks.items;
       for (let i = 0; i < songInfo.length; i++) {
         if (songInfo[i] !== undefined) {
@@ -50,24 +54,32 @@ function spotifyThisSong(songName) {
     else { return console.log(`Error occurred: ${err}`); }
   });
 };
-
+// OMDB function
 function grabMovie(movieName) {
+  // if user doesn't specify a name, Mr. Nobody is assigned to the user's input.
+  if (!movieName) {
+    movieName = "Mr. Nobody";
+  }
+  // makes request
   request(`http://www.omdbapi.com/?t=${movieName}&y=&plot=short&apikey=${ombdKey}`, function (error, response, body) {
-    // If the request is successful (i.e. if the response status code is 200)
+    // If the request is successful logs the response.
     if (!error && response.statusCode === 200) {
       const movieData = JSON.parse(body);
       console.log(`\nMovie Title: ${movieData.Title}\n\nRelease Date: ${movieData.Released}\n\nIMDB Rating: ${movieData.Ratings[0].Value}\n\nRotten Tomatoes Rating: ${movieData.Ratings[1].Value}\n\nCountry Produced In: ${movieData.Country}\n\nLanguage: ${movieData.Language}\n\nPlot: ${movieData.Plot}\n\nStarring: ${movieData.Actors}`);
     }
   });
 };
-
+// Bands in town function
 function grabConcert(artist) {
+  // makes request
   request(`https://rest.bandsintown.com/artists/${artist}/events?app_id=${bandsKey}`, function (error, response, body) {
+    // if there is no data for entered artist/band logs that they aren't playing venues
     if (!error && response.statusCode === 200) {
       const concertData = JSON.parse(body);
       if (concertData.length < 1) {
         console.log(`${artist} is not currently playing.`);
       }
+      // if artist/band has data, response is captured and logged here
       else {
         console.log(`\n${artist} is currently playing these venues:`)
         for (let i = 0; i < concertData.length; i++) {
@@ -79,8 +91,9 @@ function grabConcert(artist) {
     }
   });
 };
-
+// Do what it says function
 function doWhatItSays() {
+  // reads the random.txt file and runs the liri command within. Random.txt could be altered to run different commands. Default is spotify.
   fs.readFile("random.txt", "utf8", function (error, data) {
     if (error) {
       return console.log(error);
